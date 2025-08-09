8b166e498f2f8396bae9ac2dadd54a55610978df
Amar-04 <mulwaniamar04@gmail.com>
2025-08-09 14:32:15 +0530

    error fix

--- app/bhajans/page.js
+++ app/bhajans/page.js
@@ -4,23 +4,15 @@ import BhajanListClient from "./BhajanListClient";
 export default function BhajansPage() {
   return (
     <div>
-      <header className="bg-[#ff9933] text-white flex flex-col justify-between px-4 py-6">
+      {/* Header */}
+      <header className="bg-[#ff9933] text-white flex flex-col justify-between px-4 py-3">
         <span className="font-bold text-lg">Bhajans</span>
-        <div className="flex space-x-4">
-          {/* Search */}
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-[#ffa347] rounded-lg p-2 my-3 w-[calc(100%-1.5rem)] border-none outline-none"
-          />
-
-          <button
-            className="text-2xl font-bold rounded-full bg-white self-center text-orange-300 px-2"
-            onClick={() => setShowModal(true)}
-          >
-            +
-          </button>
-        </div>
+        {/* Search */}
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-[#ffa347] rounded-lg p-2 my-3 w-[calc(100%-1.5rem)] border-none outline-none"
+        />
       </header>
 
       <Suspense fallback={<div className="p-4">Loading...</div>}>
