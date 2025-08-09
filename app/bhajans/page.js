0bfa038b4687510535c6f6cd3054d4f10731393c
Amar-04 <mulwaniamar04@gmail.com>
2025-08-09 14:25:51 +0530

    changes

--- app/bhajans/page.js
+++ app/bhajans/page.js
@@ -4,17 +4,24 @@ import BhajanListClient from "./BhajanListClient";
 export default function BhajansPage() {
   return (
     <div>
-      {/* Header */}
-      <header className="bg-orange-500 text-white flex justify-between items-center px-4 py-3">
+      <header className="bg-[#ff9933] text-white flex flex-col justify-between px-4 py-6">
         <span className="font-bold text-lg">Bhajans</span>
-      </header>
+        <div className="flex space-x-4">
+          {/* Search */}
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-[#ffa347] rounded-lg p-2 my-3 w-[calc(100%-1.5rem)] border-none outline-none"
+          />
 
-      {/* Search */}
-      <input
-        type="text"
-        placeholder="Search"
-        className="bg-orange-200 rounded-lg p-2 m-3 w-[calc(100%-1.5rem)] border-none outline-none"
-      />
+          <button
+            className="text-2xl font-bold rounded-full bg-white self-center text-orange-300 px-2"
+            onClick={() => setShowModal(true)}
+          >
+            +
+          </button>
+        </div>
+      </header>
 
       <Suspense fallback={<div className="p-4">Loading...</div>}>
         <BhajanListClient />
